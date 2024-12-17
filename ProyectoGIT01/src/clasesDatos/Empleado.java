df5d673f210113b565074bbd4e01065456c1acd3
Jon Ibarrola <jonibarrolalol12@gmail.com>
2024-12-17 11:43:35 +0100

    Añadidos constructores

--- ProyectoGIT01/src/clasesDatos/Empleado.java
+++ ProyectoGIT01/src/clasesDatos/Empleado.java
@@ -7,4 +7,16 @@ public class Empleado {
 	private String categoria;
 	private boolean clubSocial;  //INDICA SI UN EMPLEADO ESTA EN EL CLUB SOCIAL DE LA EMPRESA
 	private float sueldo;
+	
+	public Empleado(String dni, String nombre, String apellidos, String categoria, boolean clubSocial, float sueldo) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.categoria = categoria;
+		this.clubSocial = clubSocial;
+		this.sueldo = sueldo;
+	}
 }
+
+
