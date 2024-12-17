05717513dd1b936946752d95481f4d6b638d3d74
Jon Ibarrola <jonibarrolalol12@gmail.com>
2024-12-17 10:49:30 +0100

    Añadir categoría y privacidad atributos

---
+++ ProyectoGIT01/src/clasesDatos/Empleado.java
@@ -0,0 +1,10 @@
+package clasesDatos;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private String categoria;
+	private boolean clubSocial;  //INDICA SI UN EMPLEADO ESTA EN EL CLUB SOCIAL DE LA EMPRESA
+	private float sueldo;
+}
